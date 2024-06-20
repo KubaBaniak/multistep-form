93009cd069b8a599d8a2aac19ac3e2ef5306486f
Jakub Urbaniak <kuba121201@gmail.com>
2024-06-20 08:07:57 +0200

    change error font weight

--- src/components/Errors/InputError.tsx
+++ src/components/Errors/InputError.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const ErrorMessage = styled.p`
   color: hsl(354, 84%, 57%);
+  font-weight: bold;
   margin: 0;
 `;
 export default function InputError({ errorMessage }: { errorMessage: string }) {
